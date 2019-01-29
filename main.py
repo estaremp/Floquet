1099e3f342fc9f6b4beacd526216ae164f42b83d
estaremp <mpascualestarellas@gmail.com>
2019-01-29 11:58:30 +0900

    Init

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import qutip
+#import qutip
+from qutip import *
 
+import numpy as np
+import matplotlib.pyplot as plt
 
+#This creates a new quantum Object
+print(Qobj()) #this is a 1x1 matrix with a single zero entry
+
+#Create a QObj with a user defined data
+print(Qobj([[1],[2],[3],[4],[5]])
